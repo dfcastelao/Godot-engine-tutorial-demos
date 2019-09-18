7276b9c5dd7f8a723d87f94c969b01996c768af5
DAA <dfcastelao@gmail.com>
2019-09-18 21:39:04 +0200

    steering-follow-arrive: 3rd part done: added target
     - TODO: don't let the target appear over an agent
     - TODO: bugfix whatever Nathand said at the end of the video

--- 2019/08-13-steering-follow-arrive/daa/src/Agents/CharacterArriveTo.gd
+++ 2019/08-13-steering-follow-arrive/daa/src/Agents/CharacterArriveTo.gd
@@ -8,13 +8,27 @@ const DISTANCE_THRESHOLD := 3.0
 export var slow_radius := 200.0
 export var max_speed := 500.0
 
+var target_global_position := Vector2.ZERO
 var _velocity := Vector2.ZERO
 
 
+func _ready() -> void:
+	set_physics_process(false)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("click"):
+		target_global_position = get_global_mouse_position()
+		set_physics_process(true)
+		
+
 func _physics_process(delta: float) -> void:
-	var target_global_position: Vector2 = get_global_mouse_position()
+	if Input.is_action_pressed("click"):
+		target_global_position = get_global_mouse_position()
+
 	if global_position.distance_to(target_global_position) <  DISTANCE_THRESHOLD:
-		return
+		set_physics_process(false)
+
 	_velocity =  Steering.arrive_to(
 		_velocity,
 		global_position,
