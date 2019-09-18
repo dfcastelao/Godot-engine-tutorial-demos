d37174f93635c9e92a71d49ab5f1198dfe633469
DAA <dfcastelao@gmail.com>
2019-09-18 22:51:55 +0200

    steering-follow-arrive: bugfixes for some fade_in/out errors
     - if the mouse was clicked when the cursor was over an agent, the target sprite was
       shown, despite of being in contact with it.
     - when the target was fully shown and you clicked on other part of the screen, instead
       of getting a nice fade_in, the sprite showed instantly.

--- 2019/08-13-steering-follow-arrive/daa/src/UI/Target.gd
+++ 2019/08-13-steering-follow-arrive/daa/src/UI/Target.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 
 onready var anim_player: AnimationPlayer = $AnimationPlayer
-
+onready var sprite: Sprite = $target
 
 func _ready() -> void:
 	connect("body_entered", self, "_on_body_entered")
@@ -11,11 +11,19 @@ func _ready() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("click"):
+		# DAA: this makes sure than we will have a clean "fade_in"
+		if anim_player.current_animation.empty():
+			sprite.modulate.a = 0
 		anim_player.play("fade_in")
 
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("click"):
+		# DAA: this is to avoid showing the cross when the mouse is over an agent. Not that elegant, I must say...
+		if get_overlapping_bodies():
+			anim_player.play("fade_out")
+		else:
+			anim_player.play("fade_in")
 		global_position = get_global_mouse_position()
 
 
